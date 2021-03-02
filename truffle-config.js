628d863dd12e3caa413b3ca5618ce8b138685bb9
Paqi-Aysia <j.mining20@gmail.com>
2021-03-02 12:42:40 -0600

    commit

--- truffle-config.js
+++ truffle-config.js
@@ -21,8 +21,8 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 //const infuraKey = "fj4jll3k.....";
 //
-//const fs = require('fs');
-//const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -61,13 +61,14 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
      rinkeby: {
-      provider: () => new HDWalletProvider('fresh second dove potato duck clean unveil tiny advance tide view traffic', 
+      provider: () => new HDWalletProvider(mnemonic, 
       'https://rinkeby.infura.io/v3/dbfb6280329f42b09a923228608061f1'),
       network_id: 4,       // Rinkeby's id
-      gas: 5500000,        // Rinkeby has a lower block limit than mainnet
-      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      gas: 5500000,
+      gasPrice: 88000000000,         // Rinkeby has a lower block limit than mainnet
+      //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      //timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
      },
 
     // Useful for private networks
